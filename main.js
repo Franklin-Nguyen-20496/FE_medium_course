451fb9dd06545bf8b815e9f7ec874e05ddddf68d
unknown <hoang01663011503@yahoo.com>
2021-10-28 03:57:22 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+
+const testimonialsItems = $('.testimonials__item');
+const itemsLength = testimonialsItems.length;
+
+var indexImg = 0;
+
+function changeImg(){
+    indexImg++; 
+    if (indexImg > itemsLength){
+        indexImg = 1;
+    }
+
+    $(`.testimonials__item${indexImg}`).removeClass('d-none');
+
+    setTimeout(() => {
+        $(`.testimonials__item${indexImg}`).addClass('d-none');
+        changeImg();
+    }, 3750);
+}
+
+$('document').ready(() => {
+    changeImg();
+    // console.log($('.header__menu'));
+    $('.header__responsive').click(() => {
+        $('.header__menu').toggleClass('d-none');
+    })
+})
